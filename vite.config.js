1534bd174ead8ecf295faa900da4b08afce65939
diidhul <fadhil.elsafer@gmail.com>
2025-02-03 22:49:00 +0700

    fix error vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import path from "path";
-import { fileURLToPath } from "url"; // Untuk mengonversi import.meta.url menjadi path
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
-// Konversi import.meta.url menjadi __dirname
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
